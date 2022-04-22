5f53e40f6bfc28cfa74e6961479febc6135d2dff
Juans Ochoa <sebastian.ochoa.dev@gmail.com>
2022-04-22 11:20:27 -0500

    Interfaz para listara los pokemons mediante una tabla

--- src/components/ListTablePokemons.js
+++ src/components/ListTablePokemons.js
@@ -10,6 +10,7 @@ import {
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { getPokemons } from "../services/getPokemons";
+import pokemonImage from "../assets/images/pokemon.jpg";
 
 export default function ListTablePokemons() {
   const [pokemons, setPokemons] = useState([]);
@@ -21,15 +22,14 @@ export default function ListTablePokemons() {
   }, []);
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <Table stickyHeader sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
           <TableRow>
+            <TableCell>Id</TableCell>
             <TableCell>Image</TableCell>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align='right'>Calories</TableCell>
-            <TableCell align='right'>Fat&nbsp;(g)</TableCell>
-            <TableCell align='right'>Carbs&nbsp;(g)</TableCell>
-            <TableCell align='right'>Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Weight</TableCell>
+            <TableCell>Height</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -37,21 +37,20 @@ export default function ListTablePokemons() {
             <TableRow
               key={pokemon.name}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+              <TableCell align='right'>{pokemon.id}</TableCell>
               <TableCell component='th' scope='pokemon'>
-                <Image
-                  src={pokemon.sprites.front_default}
+                <img
+                  src={pokemon.image}
+                  width='50'
+                  height='50'
                   alt={pokemon.name}
-                  width={100}
-                  height={100}
                 />
               </TableCell>
               <TableCell component='th' scope='pokemon'>
                 {pokemon.name}
               </TableCell>
-              <TableCell align='right'>Hola</TableCell>
-              <TableCell align='right'>Hola</TableCell>
-              <TableCell align='right'>Hola</TableCell>
-              <TableCell align='right'>Hola</TableCell>
+              <TableCell align='right'>{pokemon.weight}</TableCell>
+              <TableCell align='right'>{pokemon.height}</TableCell>
             </TableRow>
           ))}
         </TableBody>
