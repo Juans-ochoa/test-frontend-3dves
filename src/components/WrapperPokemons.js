78973aee56d2de9d3268ecb91803bb5d1c6bc2dc
Juans Ochoa <sebastian.ochoa.dev@gmail.com>
2022-04-23 14:20:38 -0500

    Agregando navegador para cambiar de tipo de vista en los pokemons

--- src/components/WrapperPokemons.js
+++ src/components/WrapperPokemons.js
@@ -1,8 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import ListGalleryePokemons from "./ListGalleryePokemons";
+import ListPokemons from "./ListPokemons";
 import ListTablePokemons from "./ListTablePokemons";
 
 export default function WrapperPokemons() {
   const [option, setOption] = useState(0);
 
-  return <>{option === 0 ? <ListTablePokemons /> : null}</>;
+  return (
+    <>
+      <nav className='view__pokemons'>
+        <ul>
+          <li>
+            <button onClick={() => setOption(0)} className='btn_pokemon_view'>
+              Table
+            </button>
+          </li>
+          <li>
+            <button onClick={() => setOption(1)} className='btn_pokemon_view'>
+              Gallery
+            </button>
+          </li>
+          <li>
+            <button onClick={() => setOption(2)} className='btn_pokemon_view'>
+              List
+            </button>
+          </li>
+        </ul>
+      </nav>
+      {option === 0 ? (
+        <ListTablePokemons />
+      ) : option === 1 ? (
+        <ListGalleryePokemons />
+      ) : (
+        <ListPokemons />
+      )}
+    </>
+  );
 }
