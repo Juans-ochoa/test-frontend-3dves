8e9bb3aba7e835385a3da85cd7bb606ac47eedf1
Juans Ochoa <sebastian.ochoa.dev@gmail.com>
2022-04-23 14:22:52 -0500

    Componente para le Header y Spinner

---
+++ src/components/global/Header.js
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <h1>My List Pokemons</h1>
+    </header>
+  );
+}
