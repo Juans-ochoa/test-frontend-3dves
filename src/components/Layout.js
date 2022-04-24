9b91bab23659767caec233084cd24b57045dfc78
Juans Ochoa <sebastian.ochoa.dev@gmail.com>
2022-04-24 00:19:17 -0500

    Creando Componente del footer y agregandolo al Layout principal

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,15 @@
 import { Container } from "@mui/material";
+import Footer from "./global/Footer";
 import Header from "./global/Header";
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <Container>
+      <Container style={{ minHeight: "90vh" }}>
         <main className='m-2'>{children}</main>
       </Container>
+      <Footer />
     </>
   );
 }
---
+++ src/components/global/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className='footer'>
+      <h4>Created by Juan Sebastian Ochoa</h4>
+    </footer>
+  );
+}
